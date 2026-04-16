ee9f61ec7f0671e2508d7aa866f26400768cc750
Krishnakant Pandit <krishnakantpandit35@gmail.com>
2026-04-16 10:30:52 +0530

    Delete week10_status.py

--- week10_status.py
+++
@@ -1,7 +0,0 @@
-def update_status(orders, order_id):
-    if order_id in orders:
-        new_status = input("Enter new status (Shipped/Delivered): ")
-        orders[order_id]["status"] = new_status
-        print("Order updated successfully!\n")
-    else:
-        print("Order not found!\n")
