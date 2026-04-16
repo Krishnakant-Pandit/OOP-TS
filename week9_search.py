fe5db6114f64dbb1eb606f025a7ae04160c7a797
Krishnakant Pandit <krishnakantpandit35@gmail.com>
2026-04-16 10:30:14 +0530

    Delete week9_search.py

--- week9_search.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def search_order(orders, order_id):
-    if order_id in orders:
-        df = pd.DataFrame([orders[order_id]])
-        print("\nOrder Found:\n")
-        print(df, "\n")
-    else:
-        print("Order not found!\n")
